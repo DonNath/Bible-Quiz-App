4e95d6dcea96027facfa510e495eff45a4549cf9
DonNath <nathanielayodele179@gmail.com>
2023-09-07 23:12:19 +0100

    functions to activate App

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        question:"How many stones did David pick when he killed goliath?",
+        question:"How many stones did David pick when on his way to kill goliath?",
         answers:[
             { text: "20", correct: false},
             { text: "5", correct: true},
@@ -28,184 +28,302 @@ const questions = [
             { text: "Marguire", correct: false}
             
         ]
-    },
-    {
-        question:"who was the father of Benjamin?",
+     },
+   {
+         question:"who was the father of Benjamin?",
         answers:[
-            { text: "Samuel", correct: false},
+             { text: "Samuel", correct: false},
             { text: "Jacob", correct: true},
-            { text: "Patrick", correct: false},
-            { text: "Jethro", correct: false}
+             { text: "Patrick", correct: false},
+             { text: "Jethro", correct: false}
             
-        ]
-    },
-    {
-        question:"Samson was a ________ from birth",
-        answers:[
-            { text: "Boxer", correct: false},
-            { text: "Nirvana", correct: false},
-            { text: "ishmaeli", correct: false},
-            { text: "Nazarite", correct: true}
+         ]
+     },
+     {
+         question:"Samson was a ________ from birth",
+         answers:[
+             { text: "Boxer", correct: false},
+             { text: "Nirvana", correct: false},
+             { text: "ishmaeli", correct: false},
+             { text: "Nazarite", correct: true}
             
-        ]
-    },
-    {
-        question:"which four people followed Christ to the Mount of configuration",
-        answers:[ 
-            { text: "Peter, Paul, James, John", correct: false},
-            { text: "Andrew, Peter, James, John", correct: true},
-            { text: "Samuel, Elijah, Paul, Peter", correct: false},
-            { text: "Simon, Matthew, John, Andrew", correct: false}
+         ]
+     },
+     {
+         question:"which four people followed Christ to the Mount of configuration",
+         answers:[ 
+             { text: "Peter, Paul, James, John", correct: false},
+             { text: "Andrew, Peter, James, John", correct: true},
+             { text: "Samuel, Elijah, Paul, Peter", correct: false},
+             { text: "Simon, Matthew, John, Andrew", correct: false}
             
-        ]
-    },
-    {
-        question:"Moses the son-in-Law to",
-        answers:[
+         ]
+     },
+ {
+         question:"Moses the son-in-Law to",
+         answers:[
             { text: "Jethro the priest of midian", correct: true},
-            { text: "Aphaeus the son of thunder", correct: false},
+             { text: "Aphaeus the son of thunder", correct: false},
             { text: "Elkanah the hittite", correct: false},
-            { text: "Aaron the levite", correct: false}
+          { text: "Aaron the levite", correct: false}
             
         ]
-    },
-    {
-        question:"In Mark, Jesus was walking by what sea when he saw Simon and Andrew his brother ",
-        answers:[
+     },
+     {
+         question:"In Mark, Jesus was walking by what sea when he saw Simon and Andrew his brother ",
+         answers:[
             { text: "Blue sea", correct: false},
-            { text: "Red sea", correct: false},
+             { text: "Red sea", correct: false},
             { text: "Sea of Tiberus", correct: false},
-            { text: "Sea of Galilee", correct: true}
+             { text: "Sea of Galilee", correct: true}
+            
+     ]
+     },
+     {
+         question:"Who died as a result of violating God's law concerning how to carry the Ark of Convenant?",
+    answers:[
+             { text: "Achan", correct: false},
+             { text: "Uzzah", correct: true},
+           { text: "Daniel", correct: false},
+           { text: "Uziah", correct: false}
             
         ]
-    },
-    {
-        question:"Who died as a result of violating God's law concerning how to carry the Ark of Convenant?",
-        answers:[
-            { text: "Achan", correct: false},
-            { text: "Uzzah", correct: true},
-            { text: "Daniel", correct: false},
-            { text: "Uziah", correct: false}
+     },
+     {
+   question:"He offered up his only daughter after a battle according to his promise",
+         answers:[
+             { text: "Jephtah", correct: true},
+         { text: "Jethro", correct: false},
+             { text: "John", correct: false},
+             { text: "jephuneh", correct: false}
             
         ]
-    },
-    {
-        question:"He offered up his only daughter after a battle according to his promise",
-        answers:[
-            { text: "Jephtah", correct: true},
-            { text: "Jethro", correct: false},
-            { text: "John", correct: false},
-            { text: "jephuneh", correct: false}
-            
-        ]
-    },
-    {
-        question:"Jesus told him 'an Isrealite indeed in whom there is no guile '",
-        answers:[
-            { text: "Philemon", correct: false},
+     },
+     {
+         question:"Jesus told him 'an Isrealite indeed in whom there is no guile '",
+         answers:[
+             { text: "Philemon", correct: false},
             { text: "Philip", correct: false},
-            { text: "Saul", correct: false},
-            { text: "Nathanael", correct: true}
+         { text: "Saul", correct: false},
+             { text: "Nathanael", correct: true}
             
-        ]
-    },
-    {
-    question:"The smallest city in Jerusalem? ",
+         ]
+     },
+     {
+     question:"The smallest city in Jerusalem? ",
     answers:[
-        { text: "Bethlehem Judea", correct: true},
-        { text: "Decapolis", correct: false},
-        { text: "Troas", correct: false},
-        { text: "Spain", correct: false}
+   { text: "Bethlehem Judea", correct: true},
+         { text: "Decapolis", correct: false},
+         { text: "Troas", correct: false},
+         { text: "Spain", correct: false}
         
-    ]
+     ]
     },
-    {
-    question:"How many tribe makes up israel ", 
-    answers:[
-        { text: "15", correct: false},
+     {
+     question:"How many tribe makes up israel ", 
+     answers:[
+         { text: "15", correct: false},
         { text: "11", correct: false},
-        { text: "21", correct: false},
+       { text: "21", correct: false},
         { text: "12", correct: true}
         
-    ]
-    },
-    {
-    question:"How old was Abraham when God ask him to leave his family",
-    answers:[
-        { text: "Seventy-three", correct: false},
-        { text: "Ninety-nine", correct: false},
-        { text: "Seventy-five", correct: true},
-        { text: "Eighty-eight", correct: false}
+     ]
+     },
+     {
+     question:"How old was Abraham when God ask him to leave his family",
+     answers:[
+         { text: "Seventy-three", correct: false},
+     { text: "Ninety-nine", correct: false},
+         { text: "Seventy-five", correct: true},
+         { text: "Eighty-eight", correct: false}
         
-    ]
-    },
+     ]
+     },
     {
-    question:"'Ye are to pass through the coast of your brethren the children of Esau', Who are these children of Esau?",
-    answers:[
-        { text: "Ammonites", correct: false},
-        { text: "Moabites", correct: true},
-        { text: "perrizites", correct: false},
-        { text: "Jebusites", correct: false}
+     question:"'Ye are to pass through the coast of your brethren the children of Esau', Who are these children of Esau?",
+     answers:[
+    { text: "Ammonites", correct: false},
+         { text: "Moabites", correct: true},
+         { text: "perrizites", correct: false},
+         { text: "Jebusites", correct: false}
         
-    ]
-    },
-    {
+     ]
+     },
+     {
     question: "'Let not your heart be troubled; Yea believe in God, believe also in me', can be found where in the scriptures? ",
-    answers:[
-        { text: "John 14:1", correct: true},
-        { text: "Philippians 4:1", correct: false},
-        { text: "John 4:11", correct: false},
-        { text: "John 11:14", correct: false}
+     answers:[
+         { text: "John 14:1", correct: true},
+         { text: "Philippians 4:1", correct: false},
+         { text: "John 4:11", correct: false},
+         { text: "John 11:14", correct: false}
         
-    ]
-    },
-    {
-    question:"The Beatitudes is found in what verse of the scripture?",
-    answers:[
-        { text: "Deuteronomy", correct: false},
-        { text: "Exodus", correct: false},
+     ]
+     },
+     {
+     question:"The Beatitudes is found in what verse of the scripture?",
+     answers:[
+   { text: "Deuteronomy", correct: false},
+         { text: "Exodus", correct: false},
         { text: "James", correct: false},
-        { text: "Matthew", correct: true}
+         { text: "Matthew", correct: true}
         
-    ]
+     ]
     },
-    {
-    question:"'Elijah was a man subject to like passion as we are and he prayed earnestly that it might not rain...' is a verse found in what book of the bible?",
-    answers:[
-        { text: "Philemon", correct: false},
+     {
+     question:"'Elijah was a man subject to like passion as we are and he prayed earnestly that it might not rain...' is a verse found in what book of the bible?",
+     answers:[
+         { text: "Philemon", correct: false},
         { text: "Isaiah", correct: false},
-        { text: "Revelation", correct: false},
-        { text: "James", correct: true}
+         { text: "Revelation", correct: false},
+         { text: "James", correct: true}
+ 
+     ]
+     },
+     {
+     question:"Is any among you ________? let him ________",
+ answers:[
+         { text: "afflicted, merry", correct: false},
+         { text: "sick, fight", correct: false},
+         { text: "afflicted, pray", correct: true},
+    { text: "down, shout", correct: false}
         
-    ]
+     ]
+     },
+     {
+     question:"'Their throat is an open sepulchre' who were Christ referring to?",
+     answers:[
+         { text: "Philemon", correct: false},
+         { text: "Pharisees", correct: true},
+         { text: "Saul", correct: false},
+         { text: "Angels", correct: false}
+        
+     ]
+     },
+     {
+     question:"This Queen visited king Solomon to see his wisdom",
+     answers:[
+         { text: "Queen of Sheba", correct: true},
+     { text: "Queen Jezebeel", correct: false},
+         { text: "Queen Jezreel", correct: false},
+         { text: "Queen Esther", correct: false}     
+     ]
+     },
+     {
+        question:"David went to _______ so as to escape Saul the son of ______",
+        answers:[
+            { text: "Ziklag, Kish", correct: true},
+            { text: "Bethel, Joelah", correct: false},
+            { text: "Ziklag, Jezreel", correct: false},
+            { text: "Bethlehem, Penuel", correct: false}     
+     ]
+     },
+     {
+        question:"They married Ruth and Oprah",
+        answers:[
+            { text: "Elimelech, Ashem", correct:false },
+            { text: "Mahlon, Chilion", correct: true},
+            { text: "Boaz, Balaam ", correct: false},
+            { text: "Ophni, Phinehas", correct: false}     
+            ]
     },
     {
-    question:"Is any among you ________? let him ________",
-    answers:[
-        { text: "afflicted, merry", correct: false},
-        { text: "sick, fight", correct: false},
-        { text: "afflicted, pray", correct: true},
-        { text: "down, shout", correct: false}
-        
-    ]
-    },
-    {
-    question:"'Their throat is an open sepulchre' who were Christ referring to?",
-    answers:[
-        { text: "Philemon", correct: false},
-        { text: "Philip", correct: true},
-        { text: "Saul", correct: false},
-        { text: "Nathanael", correct: false}
-        
-    ]
-    },
-    {
-    question:"This Queen visited king Solomon to see his wisdom",
-    answers:[
-        { text: "Queen of Sheba", correct: false},
-        { text: "Queen Jezebeel", correct: true},
-        { text: "Queen Jezreel", correct: false},
-        { text: "Queen Esther", correct: false}     
-    ]
+        question:"_______ Rebekah's nurse died, and was buried beneath Bethel under a/an _______ ",
+        answers:[
+            { text: "Bethel, Oak", correct: true},
+            { text: "Spain, Rock", correct: false},
+            { text: "Samaria, sea", correct: false},
+            { text: "Peniel, Oak", correct: false}     
+            ]
     }
 ]
+
+const questionElement = document.getElementById("question")
+const answerButton = document.getElementById("answer-buttons")
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+function showQuestion() {
+
+    resetState()
+
+    let currentQuestion =  questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = `${questionNo} . ${currentQuestion.question}`
+    
+    
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+    })
+    
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild)
+    }
+}
+
+
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){ 
+        selectedBtn.classList.add("correct");
+        score++
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButton.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct")
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML= `You score ${score} out of ${questions.length}!`
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block"
+}
+
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }   
+}
+
+nextButton.addEventListener("click",()=>{
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton()        
+    }else{
+        startQuiz()
+    }
+})
+
+
+startQuiz()
